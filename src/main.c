3d7efdb0f7a406123ae70f25cca4a5d35f50f7f5
John Agapeyev <jagapeyev@gmail.com>
2019-12-03 22:00:14 -0800

    PCRE2 is done but jank and broken, have fun Isaac

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
 
 #define PCRE2_CODE_UNIT_WIDTH 32
 #include <pcre2.h>
@@ -33,13 +34,64 @@ static void print_help(void) {
  * Currently this doesn't check if the new target is valid, should it?
  * Maybe add a validation flag to args and make it optional?
  */
-static void rename_symlink(const char *sym_path, const char *new_link) {
+static void rename_symlink(const char *sym_path, const char *match, const char *replacement) {
     const size_t len = strlen(sym_path);
     const char *temp_extension = ".tmp";
+    struct stat res = {0};
     char *temp_path = malloc(len + strlen(temp_extension) + 1);
+    char *temp_dest = NULL;
+    char *new_link = NULL;
+    pcre2_code *match_pattern = NULL;
+    size_t new_dest_len = 0;
+    int error_code = 0;
+    size_t error_offset = 0;
+
+    char err_msg[1024];
+
     if (temp_path == NULL) {
-        return;
+        goto error;
     }
+
+    if (lstat(sym_path, &res) == -1) {
+        perror("lstat");
+        goto error;
+    }
+
+    temp_dest = malloc(res.st_size + 1);
+    if (temp_dest == NULL) {
+        goto error;
+    }
+
+    if (readlink(sym_path, temp_dest, res.st_size + 1) == -1) {
+        perror("readlink");
+        goto error;
+    }
+    temp_dest[res.st_size] = '\0';
+
+    match_pattern = pcre2_compile((const unsigned int *) match, strlen(match),
+            (uint32_t) PCRE2_ZERO_TERMINATED, &error_code, &error_offset, NULL);
+    if (match_pattern == NULL) {
+        pcre2_get_error_message(error_code, (unsigned int *) err_msg, 1024);
+        printf("%s\n", err_msg);
+        goto error;
+    }
+
+    pcre2_substitute(match_pattern, (const unsigned int *) temp_dest, strlen(temp_dest), 0,
+            PCRE2_SUBSTITUTE_GLOBAL | PCRE2_SUBSTITUTE_EXTENDED | PCRE2_SUBSTITUTE_OVERFLOW_LENGTH,
+            NULL, NULL, (const unsigned int *) replacement, strlen(replacement), NULL, &new_dest_len);
+
+    new_link = malloc(new_dest_len + 1);
+    if (new_link == NULL) {
+        goto error;
+    }
+
+    if (pcre2_substitute(match_pattern, (const unsigned int *) temp_dest, strlen(temp_dest), 0,
+                PCRE2_SUBSTITUTE_GLOBAL | PCRE2_SUBSTITUTE_EXTENDED | PCRE2_SUBSTITUTE_OVERFLOW_LENGTH,
+                NULL, NULL, (const unsigned int *) replacement, strlen(replacement), (unsigned int *) new_link, &new_dest_len) < 0) {
+        goto error;
+    }
+
+    new_link[new_dest_len] = '\0';
 
     memcpy(temp_path, sym_path, len);
     temp_path[len] = '\0';
@@ -56,11 +108,22 @@ static void rename_symlink(const char *sym_path, const char *new_link) {
     }
 
 error:
-    free(temp_path);
+    if (temp_path) {
+        free(temp_path);
+    }
+    if (temp_dest) {
+        free(temp_dest);
+    }
+    if (match_pattern) {
+        pcre2_code_free(match_pattern);
+    }
+    if (new_link) {
+        free(new_link);
+    }
     return;
 }
 
-static void handle_file_type(const char *pathname, const char *new_link) {
+static void handle_file_type(const char *pathname, const char *match, const char *replacement) {
     struct stat res;
     if (lstat(pathname, &res) == -1) {
         perror("lstat");
@@ -70,7 +133,7 @@ static void handle_file_type(const char *pathname, const char *new_link) {
     //Stat doesn't distinguish between hard link and actual file
     if (S_ISLNK(res.st_mode)) {
         //Symlink
-        rename_symlink(pathname, new_link);
+        rename_symlink(pathname, match, replacement);
     }
     //Are we even going to try and rename regular files and/or special files like block devices?
 }
@@ -79,6 +142,10 @@ static void batch_rename(const char* match, const char* replacement, const char 
     printf("match %s replacement %s\n", match, replacement);
     for (size_t i = 0; i < n; ++i) {
         printf("%s\n", symlinks[i]);
+    }
+
+    for (size_t i = 0; i < n; ++i) {
+        handle_file_type(symlinks[i], match, replacement);
     }
 }
 
@@ -134,12 +201,13 @@ int main(int argc, char **argv) {
         target_symlink = argv[1];
         target_dest = argv[2];
     } else if (argc >= 4 && match == NULL && replacement == NULL) {
-        match = argv[0];
-        replacement = argv[1];
+        match = argv[1];
+        replacement = argv[2];
         optind += 2;
     }
 
 
+#if 0
     if (match == NULL && replacement == NULL && target_symlink != NULL && target_dest != NULL) {
         //single mode
         if (optind < argc) {
@@ -158,6 +226,16 @@ int main(int argc, char **argv) {
         print_help();
         exit(EXIT_SUCCESS);
     }
+#else
+    if (match != NULL && replacement != NULL) {
+        //batch mode
+        batch_rename(match, replacement, (const char **)argv+optind, argc-optind);
+    } else {
+        //you messed up
+        print_help();
+        exit(EXIT_SUCCESS);
+    }
+#endif
 
     return EXIT_SUCCESS;
 }
