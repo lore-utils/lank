e566f630ff0abce32387ca278e568bd2deddcb9e
Isaac Morneau <isaacmorneau@gmail.com>
2019-12-03 22:37:25 -0800

    reenabled both single and batch rename

--- src/main.c
+++ src/main.c
@@ -1,56 +1,77 @@
 #include <getopt.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <string.h>
-#include <stdint.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
 
 static void print_help(void) {
-    printf(
-        "lank - a symlink utility for all things symlink\n"
-        "Usage:\n"
-        "    lank SRC DEST\n"
-        "    lank MATCH REPLACEMENT FILES...\n"
-        "    lank <OPTIONS>\n"
-        "Options:\n"
-        "    -[-s]rc - the src symlink file to modify\n"
-        "    -[-d]est - the new destination for the input symlink\n"
-        "\n"
-        "    -[-m]atch - the regex pattern to apply to the target of the symlinks\n"
-        "    -[-r]eplacement - the replacement pattern for the match\n"
-        "\n"
-        "    -[-h]elp - this message\n"
-        "Notes:\n"
-        "    Uses the PCRE2 regex engine for replacements.\n"
-        );
+    printf("lank - a symlink utility for all things symlink\n"
+           "Usage:\n"
+           "    lank SRC DEST\n"
+           "    lank MATCH REPLACEMENT FILES...\n"
+           "    lank <OPTIONS>\n"
+           "Options:\n"
+           "    -[-s]rc - the src symlink file to modify\n"
+           "    -[-d]est - the new destination for the input symlink\n"
+           "\n"
+           "    -[-m]atch - the regex pattern to apply to the target of the symlinks\n"
+           "    -[-r]eplacement - the replacement pattern for the match\n"
+           "\n"
+           "    -[-h]elp - this message\n"
+           "Notes:\n"
+           "    Uses the PCRE2 regex engine for replacements.\n");
 }
 
 /*
  * Currently this doesn't check if the new target is valid, should it?
  * Maybe add a validation flag to args and make it optional?
  */
-static void rename_symlink(const char *sym_path, const char *match, const char *replacement) {
-    const size_t len = strlen(sym_path);
+static void rename_symlink(const char *sym_path, const char *new_link) {
+    const size_t len           = strlen(sym_path);
     const char *temp_extension = ".tmp";
-    struct stat res = {0};
-    char *temp_path = malloc(len + strlen(temp_extension) + 1);
-    char *temp_dest = NULL;
-    char *new_link = NULL;
-    pcre2_code *match_pattern = NULL;
-    size_t new_dest_len = 0;
-    int error_code = 0;
-    size_t error_offset = 0;
-
-    char err_msg[120];
-
+    char *temp_path            = malloc(len + strlen(temp_extension) + 1);
     if (temp_path == NULL) {
+        return;
+    }
+
+    memcpy(temp_path, sym_path, len);
+    temp_path[len] = '\0';
+    strcat(temp_path, temp_extension);
+
+    if (symlink(new_link, temp_path) == -1) {
+        perror("symlink");
         goto error;
     }
+
+    if (rename(temp_path, sym_path) == -1) {
+        perror("rename");
+        goto error;
+    }
+
+error:
+    free(temp_path);
+    return;
+}
+
+/**
+ *  Fills newlink with a null terminated string for renaming
+ */
+static void re_path(
+    const char *sym_path, const char *match, const char *replacement, char **new_link) {
+    struct stat res           = {0};
+    char *temp_dest           = NULL;
+    pcre2_code *match_pattern = NULL;
+    size_t new_dest_len       = 0;
+    int error_code            = 0;
+    size_t error_offset       = 0;
+
+    char err_msg[120];
 
     if (lstat(sym_path, &res) == -1) {
         perror("lstat");
@@ -68,84 +89,66 @@ static void rename_symlink(const char *sym_path, const char *match, const char *
     }
     temp_dest[res.st_size] = '\0';
 
-    match_pattern = pcre2_compile((const unsigned char *) match, strlen(match),
-            0, &error_code, &error_offset, NULL);
+    match_pattern
+        = pcre2_compile((PCRE2_SPTR)match, strlen(match), 0, &error_code, &error_offset, NULL);
     if (match_pattern == NULL) {
-        pcre2_get_error_message(error_code, (unsigned char *) err_msg, sizeof(err_msg));
+        pcre2_get_error_message(error_code, (PCRE2_UCHAR *)err_msg, sizeof(err_msg));
         printf("%s\n", err_msg);
         goto error;
     }
 
-    pcre2_substitute(match_pattern, (const unsigned char *) temp_dest, strlen(temp_dest), 0,
+    pcre2_substitute(match_pattern, (PCRE2_SPTR)temp_dest, strlen(temp_dest), 0,
+        PCRE2_SUBSTITUTE_GLOBAL | PCRE2_SUBSTITUTE_EXTENDED | PCRE2_SUBSTITUTE_OVERFLOW_LENGTH,
+        NULL, NULL, (PCRE2_SPTR)replacement, strlen(replacement), NULL, &new_dest_len);
+
+    *new_link = malloc(new_dest_len + 1);
+    if (*new_link == NULL) {
+        goto error;
+    }
+
+    if (pcre2_substitute(match_pattern, (PCRE2_SPTR)temp_dest, strlen(temp_dest), 0,
             PCRE2_SUBSTITUTE_GLOBAL | PCRE2_SUBSTITUTE_EXTENDED | PCRE2_SUBSTITUTE_OVERFLOW_LENGTH,
-            NULL, NULL, (const unsigned char *) replacement, strlen(replacement), NULL, &new_dest_len);
-
-    new_link = malloc(new_dest_len + 1);
-    if (new_link == NULL) {
+            NULL, NULL, (PCRE2_SPTR)replacement, strlen(replacement), (PCRE2_UCHAR *)*new_link,
+            &new_dest_len)
+        < 0) {
         goto error;
     }
 
-    if (pcre2_substitute(match_pattern, (const unsigned char *) temp_dest, strlen(temp_dest), 0,
-                PCRE2_SUBSTITUTE_GLOBAL | PCRE2_SUBSTITUTE_EXTENDED | PCRE2_SUBSTITUTE_OVERFLOW_LENGTH,
-                NULL, NULL, (const unsigned char *) replacement, strlen(replacement), (unsigned char *) new_link, &new_dest_len) < 0) {
-        goto error;
-    }
-
-    new_link[new_dest_len] = '\0';
-
-    memcpy(temp_path, sym_path, len);
-    temp_path[len] = '\0';
-    strcat(temp_path, temp_extension);
-
-    if (symlink(new_link, temp_path) == -1) {
-        perror("symlink");
-        goto error;
-    }
-
-    if (rename(temp_path, sym_path) == -1) {
-        perror("rename");
-        goto error;
-    }
+    (*new_link)[new_dest_len] = '\0';
 
 error:
-    if (temp_path) {
-        free(temp_path);
-    }
     if (temp_dest) {
         free(temp_dest);
     }
     if (match_pattern) {
         pcre2_code_free(match_pattern);
     }
-    if (new_link) {
-        free(new_link);
-    }
     return;
 }
 
-static void handle_file_type(const char *pathname, const char *match, const char *replacement) {
+static void single_rename(const char *pathname, const char *replacement) {
     struct stat res;
     if (lstat(pathname, &res) == -1) {
         perror("lstat");
         return;
     }
 
-    //Stat doesn't distinguish between hard link and actual file
     if (S_ISLNK(res.st_mode)) {
-        //Symlink
-        rename_symlink(pathname, match, replacement);
+        rename_symlink(pathname, replacement);
+    } else {
+        fprintf(stderr, "%s is not a symlink\n", pathname);
     }
-    //Are we even going to try and rename regular files and/or special files like block devices?
 }
 
-static void batch_rename(const char* match, const char* replacement, const char **symlinks, const size_t n) {
-    printf("match %s replacement %s\n", match, replacement);
+static void batch_rename(
+    const char *match, const char *replacement, const char **symlinks, const size_t n) {
     for (size_t i = 0; i < n; ++i) {
-        printf("%s\n", symlinks[i]);
-    }
-
-    for (size_t i = 0; i < n; ++i) {
-        handle_file_type(symlinks[i], match, replacement);
+        char *path = NULL;
+        re_path(symlinks[i], match, replacement, &path);
+        if (path) {
+            single_rename(symlinks[i], path);
+            free(path);
+        }
     }
 }
 
@@ -156,22 +159,18 @@ int main(int argc, char **argv) {
     }
 
     const char *target_symlink = NULL;
-    const char *target_dest = NULL;
-    const char *match = NULL;
-    const char *replacement= NULL;
+    const char *target_dest    = NULL;
+    const char *match          = NULL;
+    const char *replacement    = NULL;
 
     int c;
     while (1) {
         int option_index = 0;
 
-        static struct option long_options[] = {
-            {"help", no_argument, 0, 'h'},
-            {"src", required_argument, 0, 's'},
-            {"dest", required_argument, 0, 'd'},
-            {"match", required_argument, 0, 'm'},
-            {"replacement", required_argument, 0, 'r'},
-            {0, 0, 0, 0}
-        };
+        static struct option long_options[]
+            = {{"help", no_argument, 0, 'h'}, {"src", required_argument, 0, 's'},
+                {"dest", required_argument, 0, 'd'}, {"match", required_argument, 0, 'm'},
+                {"replacement", required_argument, 0, 'r'}, {0, 0, 0, 0}};
 
         c = getopt_long(argc, argv, "hs:d:m:r:", long_options, &option_index);
         if (c == -1)
@@ -197,18 +196,17 @@ int main(int argc, char **argv) {
         }
     }
 
-    if (argc == 3 && target_dest == NULL && target_symlink) {
+    if (argc == 3 && target_dest == NULL && target_symlink == NULL) {
         target_symlink = argv[1];
-        target_dest = argv[2];
+        target_dest    = argv[2];
+        optind += 2;
     } else if (argc >= 4 && match == NULL && replacement == NULL) {
-        match = argv[1];
+        match       = argv[1];
         replacement = argv[2];
         optind += 2;
     }
 
-
-#if 0
-    if (match == NULL && replacement == NULL && target_symlink != NULL && target_dest != NULL) {
+    if (target_symlink != NULL && target_dest != NULL && match == NULL && replacement == NULL) {
         //single mode
         if (optind < argc) {
             printf("ignoring args: ");
@@ -217,25 +215,16 @@ int main(int argc, char **argv) {
             }
             printf("\n");
         }
-        handle_file_type(target_symlink, target_dest);
+        single_rename(target_symlink, target_dest);
     } else if (match != NULL && replacement != NULL) {
         //batch mode
-        batch_rename(match, replacement, (const char **)argv+optind, argc-optind);
+        batch_rename(match, replacement, (const char **)argv + optind, argc - optind);
     } else {
+        printf("what %s %s %s %s\n", target_symlink, target_dest, match, replacement);
         //you messed up
         print_help();
         exit(EXIT_SUCCESS);
     }
-#else
-    if (match != NULL && replacement != NULL) {
-        //batch mode
-        batch_rename(match, replacement, (const char **)argv+optind, argc-optind);
-    } else {
-        //you messed up
-        print_help();
-        exit(EXIT_SUCCESS);
-    }
-#endif
 
     return EXIT_SUCCESS;
 }
